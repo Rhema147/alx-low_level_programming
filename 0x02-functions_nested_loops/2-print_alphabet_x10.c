c806dc0e20ac45e1ab0a49417ae7aa605bc81366
Rhema147 <rhemachi@gmail.com>
2022-10-18 10:11:04 -0700

    while loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabetsbets times ten
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+		_putchar(alpha);
+	        alpha++
+		}
+
+	_putchar('\n');
+        i++;
+	}
+}	
