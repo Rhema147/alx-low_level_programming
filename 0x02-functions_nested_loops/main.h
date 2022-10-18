e739de8a67ee9eed9ecec546c9773e0328375047
Rhema147 <rhemachi@gmail.com>
2022-10-18 09:32:36 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,14 @@
 #define MAIN_H
 
 char _putchar(char c);
+
 void print_alphabet(void);
+/** print_alphabet - print lowercase alphabet **/
+
 void print_alphabet_x10(void);
+/** print_alphabet_x10 - print lowercase alphabet ten times **/
+
+
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
